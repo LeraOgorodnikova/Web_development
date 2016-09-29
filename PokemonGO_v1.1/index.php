7a30078ce88283f7ded9f93deb5cda517b783e7f
LeraOgorodnikova <lera.ogorodnikovaa@yandex.ru>
2016-09-29 17:26:22 +0500

    применяем БЭМ

--- PokemonGO_v1.1/index.php
+++ PokemonGO_v1.1/index.php
@@ -4,13 +4,18 @@
   <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
   <title>PokemonGO</title>
   <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
+ </head>
 <body>
-<ul id="menu">
+ <style>body{
+ 	background-image: url(css/background/back.jpg);
+ 	color: #fff;
+ 	}
+ </style>
+<ul class="menu">
 <li><a href="title.html"><img src="https://1.bp.blogspot.com/-ULz9dE8cz20/V7ADa_msnAI/AAAAAAAABt8/AJOlI64e9yg3aRexEsfgCvVHJElIKk65gCPcB/s400/pokemon_go_logo.png" alt="PokemonGO"> </li></a>
- <li><a href="start.html" class="button">Start</a> </li>
- <li><a href="about.html" class="button">About</a></li>
- <li><a href="top_list.html" class="button">Top List</a></li>
+ <li><a href="start.html" class="menu-item">Start</a> </li>
+ <li><a href="about.html" class="menu-item">About</a></li>
+ <li><a href="top_list.html" class="menu-item">Top List</a></li>
 </ul>
 </body>
 </html>
