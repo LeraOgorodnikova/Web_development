aecbac613212077e0517a460bb41d950756b30be
LeraOgorodnikova <lera.ogorodnikovaa@yandex.ru>
2016-10-02 21:04:14 +0500

    исправлены ошибки

--- PokemonGO_v1.1/Content.php
+++ PokemonGO_v1.1/Content.php
@@ -2,51 +2,61 @@
 class Content
 {
     static $items=[
-            1=>["id"=>1,
-            "title"=>"index.html",
+            1=>["title"=>"index.html",
             "path"=>"index.php",
-            "href"=>"index.php?page=1"],
+            "href"=>"index.php?page=1",
+            "bodyImage"=>"css/background/back.jpg"],
 
-            2=>["id"=>2,
-            "title"=>"start",
+            2=>["title"=>"start",
             "path"=>"assets/start.html",
-            "class"=>"menu-item",
-                "href"=>"index.php?page=2"],
+            "class"=>"menu__item",
+            "href"=>"index.php?page=2",
+            "bodyImage"=>"css/background/start_image.png"],
 
-            3=>["id"=>3,
-            "title"=>"about",
+            3=>["title"=>"about",
             "path"=>"assets/about.html",
-            "class"=>"menu-item",
-                "href"=>"index.php?page=3"],
+            "class"=>"menu__item",
+            "href"=>"index.php?page=3",
+            "bodyImage"=>"css/background/about_image.jpg"],
 
-            4=>["id"=>4,
-            "title"=>"top list",
+            4=>["title"=>"top list",
             "path"=>"assets/top_list.html",
-            "class"=>"menu-item",
-                "href"=>"index.php?page=4"]
+            "class"=>"menu__item",
+            "href"=>"index.php?page=4",
+            "bodyImage"=>"css/background/top_list_image.png"]
     ];
     public static function getPage($numPage){
-        if (($numPage>0) && ($numPage <5))
-          return file_get_contents(Content::$items[$numPage]['path']);
+        if (array_key_exists ( $numPage , self::$items)==true){
+            $image=self::getBodyImage($numPage);
+            $content="<style>body{background-image:url($image);color:#fff;overflow:hidden;}</style>";
+            $content.=file_get_contents(Content::$items[$numPage]['path']);
+        }
+        return $content;
+    }
+    public static function getBodyImage($numPage){
+        return self::$items[$numPage]['bodyImage'];
+    }
+    public static function getLogo(){
+        $value=self::$items[1];
+        $answer="<a href={$value['href']}><img src=\"https://1.bp.blogspot.com/-ULz9dE8cz20/V7ADa_msnAI/AAAAAAAABt8/AJOlI64e9yg3aRexEsfgCvVHJElIKk65gCPcB/s400/pokemon_go_logo.png\"></a>";
+        return $answer;
     }
     public static function getMenu($number){
-        $newItem=Content::$items;
-        $answer="<ul class=\"menu\"><li><a href={$newItem[1]['href']}><img src=\"https://1.bp.blogspot.com/-ULz9dE8cz20/V7ADa_msnAI/AAAAAAAABt8/AJOlI64e9yg3aRexEsfgCvVHJElIKk65gCPcB/s400/pokemon_go_logo.png\"> </li></a>";
-        foreach($newItem as $key => $value){
-            foreach($value as $key_value => $line) {
-                if (@$number == @$line) {
-                    $newItem[$number]['class']="menu-item-disable";
-                }
+        $answer =self::getLogo();
+        $answer.="<ul class=\"menu\">";
+        foreach(self::$items as $key => $value){
+            if ($key!=1) {
+                    if ($number == $key) {
+                        $answer.="<li><a href="."{$value['href']}"." class="."menu__item-disable".">"."{$value['title']}"."</a></li>";
+                    }
+                    else{
+                        $answer.="<li><a href="."{$value['href']}"." class="."{$value['class']}".">"."{$value['title']}"."</a></li>";
+
+                    }
             }
-            if ($key!=1) $answer.="<li><a href="."{$newItem[$key]['href']}"." class="."{$newItem[$key]['class']}".">"."{$newItem[$key]['title']}"."</a></li>";
         }
+        $answer.="</ul>";
         return $answer;
-//        echo "<ul class=\"menu\">
-// <li><a href={$newItem[1]['href']}><img src=\"https://1.bp.blogspot.com/-ULz9dE8cz20/V7ADa_msnAI/AAAAAAAABt8/AJOlI64e9yg3aRexEsfgCvVHJElIKk65gCPcB/s400/pokemon_go_logo.png\"> </li></a>
-// <li><a href={$newItem[2]['href']} class={$newItem[2]['class']}>{$newItem[2]['title']}</a> </li>
-// <li><a href={$newItem[3]['href']} class={$newItem[3]['class']}>{$newItem[3]['title']}</a></li>
-// <li><a href={$newItem[4]['href']} class={$newItem[4]['class']}>{$newItem[4]['title']}</a></li>
-//</ul>";
 }
 }
 ?>
--- PokemonGO_v1.1/index.php
+++ PokemonGO_v1.1/index.php
@@ -2,16 +2,15 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
-
   <title>PokemonGO</title>
   <link rel="stylesheet" type="text/css" href="css/style.css">
  </head>
 <body>
- <style>body{
- 	background-image: url(css/background/back.jpg);
- 	color: #fff;
- 	}
- </style>
- <?php include ('Content.php');echo Content::getMenu($_GET['page']); echo Content::getPage($_GET['page']);?>
+<?php
+ if (isset($_GET['page'])!=true){
+     $_GET['page']=1;
+ }
+ include ('Content.php');echo Content::getMenu($_GET['page']); echo Content::getPage($_GET['page']);
+?>
 </body>
 </html>
