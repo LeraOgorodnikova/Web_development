940a4db87936fcd7000b3aee24956182628538b7
LeraOgorodnikova <lera.ogorodnikovaa@yandex.ru>
2016-11-05 18:33:55 +0500

    попытка сделать саму игру с покемонами

--- pokemonGO_Angular/js/app.js
+++ pokemonGO_Angular/js/app.js
@@ -34,26 +34,62 @@ app.config(function($routeProvider) {
 		    }
 		}
 	})
-.controller("gameController",function ($scope,$log,$rootScope,$routeParams) {
+// .value('pokConf', {
+//   power: 0,
+//   speed: 0,
+//   width: 100 ,
+//   height: 100 ,
+//   image: ""
+// })
+.value("confPower", 0)
+.value("confSpeed", 0)
+.value("confWidth", 100)
+.value("confHeight", 100)
+.value("confImage", "")
+.controller("gameController",function ($scope,$log,$rootScope,$routeParams,$interval) {
 	$scope.ballPos={'X':0,'Y':0};
-
+    $scope.score=0;
+    $scope.level=1;
+    $scope.time=0;
     var tictac, tic=0;
-		$scope.start=function(){
+	$scope.start=function(){
+		
 		tictac=$interval(function(){
 			tic++;
 			$scope.ballPos.X=50*Math.sin(tic/50);
 			$scope.ballPos.Y=20*Math.cos(tic/20);
 		},50);	
 	};
+		$scope.stop=function(){
+		$interval.cancel(tictac);
+	};
+		$rootScope.$on('catch',function(){
+		$scope.level++;
+        $scope.score+= confPower*tic*confSpeed;
+	});
 	})
 .directive("pokemonDirective" , function(){
 		return {
 			restrict: 'E',
 			templateUrl:"assets/directives/pokemon.html",
 			replace: true,
-			scope:{
-			current:'='
+		    controller: function($scope){
+		        $scope.catch=function(){
+		        $rootScope.$emit('catch');
+	            };
 		    },
-		    //code
+		    controller:function($scope, $http){
+                $http.get("?controller=pokemon").then(function (response) {
+                var newArray = response.data;
+                confPower=newArray["power"];
+                confSpeed=newArray["speed"];
+                confImage=newArray["image"];
+                });
+		    },
+            scope:{
+			pokwidth: confWidth,
+            pokheight: confHeight,
+            imagePokemon: confImage
+		    }
 		}
 	})
